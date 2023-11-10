804d80c6cccafa9cc389b07856543e172f969347
Afshin Jalili <a.jalili@kaman.ir>
2023-11-10 12:30:16 +0330

    feat(): Add dynamic array implementation

--- basics/dynamic-array.ts
+++ basics/dynamic-array.ts
@@ -15,33 +15,49 @@ interface IDynamicArray {
 }
 
 class DynamicArray implements IDynamicArray {
-  constructor(private capacity: number) {}
+  private array: Array<number>;
+
+  constructor(private capacity: number) {
+    if (this.capacity <= 0) {
+      throw new Error("capacity must be greater than zero");
+    }
+    this.array = new Array<number>(this.capacity);
+  }
 
   get(index: number): number {
-    throw new Error("Method not implemented.");
+    return this.array[index];
   }
 
   set(index: number, value: number): void {
-    throw new Error("Method not implemented.");
+    this.array[index] = value;
   }
 
   pushback(value: number): void {
-    throw new Error("Method not implemented.");
+    if (this.isFull()) {
+      this.resize();
+    }
+    this.array.push(value);
   }
 
   popback(): number {
-    throw new Error("Method not implemented.");
+    return this.array.pop() ?? NaN;
   }
 
   resize(): void {
-    throw new Error("Method not implemented.");
+    this.capacity *= 2;
   }
 
   getSize(): number {
-    throw new Error("Method not implemented.");
+    return this.array.reduce((acc: number, currentValue: number): number => {
+      return typeof currentValue === "number" ? ++acc : acc;
+    }, 0);
   }
 
   getCapacity(): number {
-    throw new Error("Method not implemented.");
+    return this.capacity;
+  }
+
+  private isFull() {
+    return typeof this.array[this.capacity - 1] === "number";
   }
 }
